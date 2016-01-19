2d5b5edde45fd910ea4dd6e169c2f5a5b6f0aefd
Markus Hjort <markus.hjort@gmail.com>
2016-01-19 07:01:39 +0200

    Load file from s3 and generate gatling chart

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
+                 [clj-gatling "0.7.4"]
                  [amazonica "0.3.48"]])
--- src/clojider_remote/core.clj
+++ src/clojider_remote/core.clj
@@ -1,7 +1,9 @@
 (ns clojider-remote.core
  (:require [clojure.edn :as edn]
            [clojure.java.io :as io]
+           [clj-gatling.chart :as chart]
            [cheshire.core :refer [generate-string parse-stream]]
+           [amazonica.aws.s3 :as s3]
            [amazonica.aws.lambda :as lambda]))
 
 (def creds (edn/read-string (slurp "config.edn")))
@@ -17,13 +19,24 @@
 (defn generate-payload [o]
   (java.nio.ByteBuffer/wrap (.getBytes (generate-string o) "UTF-8")))
 
+(defn create-dir [dir]
+    (.mkdirs (java.io.File. dir)))
 
 (def scenario
   {:name "Ping scenario"
    :requests [{:name "Ping" :http "http://clj-gatling-demo-server.herokuapp.com/ping"}]})
 
+(defn download-file [results-dir bucket object-key]
+  (io/copy (:object-content (s3/get-object creds bucket object-key))
+           (io/file (str results-dir "/" object-key))))
+
+;(do
+;  (create-dir "tmp/input")
+;  (download-file "tmp/input" "clojider-results" "simulation0.log")
+;  (chart/create-chart "tmp"))
+
 ;(parse-result (lambda/invoke creds :function-name "clojider-development-lambda"
 ;                             :payload (generate-payload {:scenarios [scenario]
 ;                                                         :users 10
-;                                                         :options {:requests 1000}})))
+;                                                         :options {:requests 100}})))
 
